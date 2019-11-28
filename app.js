32d5ebdb16bc2aa243e3a02eefd6cb0aea542f1d
chjserver <953222186@qq.com>
2019-11-28 12:17:22 +0800

    asd

---
+++ app.js
@@ -0,0 +1,32 @@
+new Vue({
+    el: "#app",
+    data: {
+        theme: './img/1.jpg',
+        arr: [
+            {
+                imgUrl: './img/tmall.png',
+                title: '韩都衣舍...',
+                shop: '韩都衣舍旗舰店',
+                share: '分享赚3.68元',
+                price: 88,
+                sell: 8901,
+            },
+            {
+                imgUrl: './img/tmall.png',
+                title: '韩都衣舍...',
+                shop: '韩都衣舍旗舰店',
+                share: '分享赚3.68元',
+                price: 88,
+                sell: 8901,
+            }, 
+            {
+                imgUrl: './img/tmall.png',
+                title: '韩都衣舍...',
+                shop: '韩都衣舍旗舰店',
+                share: '分享赚3.68元',
+                price: 88,
+                sell: 8901,
+            },
+        ]
+    }
+})
